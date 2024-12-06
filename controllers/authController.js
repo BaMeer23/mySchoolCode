f6071b4613d8f7cd66e71d385f2929feb780ac3f
BaMeer23 <romeerjohnvillanueva062404@gmail.com>
2024-12-06 12:38:12 +0800

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(passwordx, 10);
         const [rows] = await pool.query('INSERT INTO users (fullname, username, passwordx) VALUES (?, ?, ?)', [fullname, username, hashedPassword]);
 
-        res.status(201).json({ message: 'Register kana' });
+        res.status(201).json({ message: 'Successfully Register' });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -22,14 +22,14 @@ const login = async (req, res) => {
         const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
 
         if (rows.length === 0) {
-            return res.status(400).json({ error: 'username mo mali' });
+            return res.status(400).json({ error: 'Wrong username' });
         }
 
         const user = rows[0];
         const isMatch = await bcrypt.compare(passwordx, user.passwordx);
 
         if (!isMatch) {
-            return res.status(400).json({ error: 'Alalanin mo password mo' });
+            return res.status(400).json({ error: 'Wrong password' });
         }
 
         const expiresIn = process.env.JWT_ACCESS_EXPIRATION_TIME || '24h';
